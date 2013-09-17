2335dd7bf58ea186688bd1b9617d3cb14eeb7d8a
James deBoer <james@huronbox.com>
2013-09-16 18:50:43 -0700

    fixup

--- perf/parser_perf.dart
+++ perf/parser_perf.dart
@@ -4,61 +4,7 @@ import '../test/parser/generated_functions.dart' as generated_functions;
 import '../test/parser/generated_getter_setter.dart' as generated_getter_setter;
 import 'package:intl/intl.dart';
 
-main() {
-  var injector = new DynamicInjector(
-      modules: [new Module()
-        ..type(Parser, implementedBy: DynamicParser)],
-      allowImplicitInjection:true);
-  var scope = injector.get(Scope);
-  var reflectiveParser = injector.get(Parser);
-  var generatedParser = new DynamicInjector(
-      modules: [new Module()
-        ..type(Parser, implementedBy: StaticParser)
-        ..value(StaticParserFunctions, generated_functions.functions())],
-      allowImplicitInjection:true).get(Parser);
-  var hybridParser = new DynamicInjector(
-      modules: [new Module()
-        ..type(Parser, implementedBy: DynamicParser)
-        ..type(GetterSetter, implementedBy: generated_getter_setter.StaticGetterSetter)],
-      allowImplicitInjection:true).get(Parser);
 
-  scope['a'] = new ATest();
-  scope['e1'] = new EqualsThrows();
-
-  compare(expr, idealFn) {
-    var nf = new NumberFormat.decimalPattern();
-    var reflectionExpr = reflectiveParser(expr);
-    var generatedExpr = generatedParser(expr);
-    var hybridExpr = hybridParser(expr);
-    var measure = (b) => statMeasure(b).mean_ops_sec;
-    var gTime = measure(() => generatedExpr.eval(scope));
-    var rTime = measure(() => reflectionExpr.eval(scope));
-    var hTime = measure(() => hybridExpr.eval(scope));
-    var iTime = measure(() => idealFn(scope));
-    print('$expr => g: ${nf.format(gTime)} ops/sec   ' +
-          'r: ${nf.format(rTime)} ops/sec   ' +
-          'h: ${nf.format(hTime)} ops/sec   ' +
-          'i: ${nf.format(iTime)} ops/sec = ' +
-          'i/g: ${nf.format(iTime / gTime)} x  ' +
-          'i/r: ${nf.format(iTime / rTime)} x  ' +
-          'i/h: ${nf.format(iTime / hTime)} x  ' +
-          'g/h: ${nf.format(gTime / hTime)} x  ' +
-          'h/r: ${nf.format(hTime / rTime)} x  ' +
-          'g/r: ${nf.format(gTime / rTime)} x');
-  }
-
-  compare('a.b.c', (scope) => scope['a'].b.c);
-  compare('e1.b', (scope) => scope['e1'].b);
-  compare('null', (scope) => null);
-  compare('x.b.c', (s, [l]) {
-    if (l != null && l.containsKey('x')) s = l['x'];
-    else if (s != null ) s = s is Map ? s['x'] : s.x;
-    if (s != null ) s = s is Map ? s['b'] : s.b;
-    if (s != null ) s = s is Map ? s['c'] : s.c;
-    return s;
-  });
-  compare('doesNotExist', (scope) => scope['doesNotExists']);
-}
 
 
 class ATest {
