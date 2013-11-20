a7750adbe93015c3869b5349256c9a8a35ac8d65
James deBoer <james@huronbox.com>
2013-11-20 15:32:10 -0800

    gen.sh updates

--- demo/todo/gen.sh
+++ demo/todo/gen.sh
@@ -2,8 +2,10 @@
 
 set -evx
 
-dart2js main.dart
-sed -e 's/^\s*\([\$A-Za-z0-9_]*\): function(/\1: function \1(/' out.js >out_ann.js
+#dart2js main.dart
+sed -e 's/^\s*\([0-9]\+\)\s*\([\$A-Za-z0-9_]*\): function(/ 4\t\2: function \2_\1(/' out_numbered.js >out_na.js
+
+sed -e 's/^\s*[0-9]\+\t//' out_na.js >out_ann.js
 wtf-instrument --track-heap out_ann.js main.dart.js
 
 
