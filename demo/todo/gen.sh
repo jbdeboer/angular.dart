b508b5f370917c0ca3304b69f48eeb85aae15aa7
James deBoer <james@huronbox.com>
2013-11-20 12:49:53 -0800

    more wtf work

--- demo/todo/gen.sh
+++ demo/todo/gen.sh
@@ -2,7 +2,7 @@
 
 set -evx
 
-#dart2js main.dart
+dart2js main.dart
 sed -e 's/^\s*\([\$A-Za-z0-9_]*\): function(/\1: function \1(/' out.js >out_ann.js
 wtf-instrument --track-heap out_ann.js main.dart.js
 
