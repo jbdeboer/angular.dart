8f3de455faaad9315a4e1b82e54d3920d941cf39
Paul Rohde <paul@paulrohde.com>
2014-05-16 16:56:26 -0700

    .

--- lib/core/module.dart
+++ lib/core/module.dart
@@ -64,7 +64,7 @@ export "package:angular/core/module_internal.dart" show
     ExceptionHandler,
     Interpolate,
     VmTurnZone,
-    Platform,
+    WebPlatform,
     PrototypeMap,
     RootScope,
     Scope,
